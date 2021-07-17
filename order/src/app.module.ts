f48af0dff58f6522f87de47dd902d39b3d8b950a
LuizEduardoCardozo <eduard.cardoz@gmail.com>
2021-07-16 23:27:39 -0300

    chore: add typeorm and env file configuration on orders api

--- order/src/app.module.ts
+++ order/src/app.module.ts
@@ -1,9 +1,26 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { OrderModule } from './order/order.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+    }),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'orders-db',
+      port: 3306,
+      username: 'root',
+      password: 'root',
+      database: 'micro_orders',
+      entities: [OrderModule],
+    }),
+    OrderModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
