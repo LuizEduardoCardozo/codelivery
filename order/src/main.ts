ecffd120341b75c8f92be4b2bae8c8134d78b621
LuizEduardoCardozo <eduard.cardoz@gmail.com>
2021-07-16 23:26:53 -0300

    chore: add handle-bars configurations

--- order/src/main.ts
+++ order/src/main.ts
@@ -1,8 +1,28 @@
 import { NestFactory } from '@nestjs/core';
+import * as handleBars from 'express-handlebars';
+import * as handleBarsHelpers from 'handlebars-helpers';
+import path from 'path';
+import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const { MICRO_MAPPING_URL, PORT } = process.env;
+
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  const { engine: hbsEngine } = handleBars.create({
+    helpers: {
+      ...handleBarsHelpers(),
+      MICRO_MAPPING_URL: () => MICRO_MAPPING_URL,
+    },
+    extname: '.hbs',
+    defaultLayout: 'layout',
+    partialsDir: [path.join(__dirname, '..', 'views')],
+  });
+
+  app.engine('hbs', hbsEngine);
+  app.setViewEngine('hbs');
+
+  await app.listen(PORT || 3000);
 }
 bootstrap();
