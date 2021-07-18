97486d5dfddeef31debf6598a484fcecfb0c0857
LuizEduardoCardozo <eduard.cardoz@gmail.com>
2021-07-17 21:45:45 -0300

    chore: add order module

--- order/src/order/order.module.ts
+++ order/src/order/order.module.ts
@@ -1,4 +1,12 @@
-import { Module } from '@nestjs/common';
+import { HttpModule, Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DriverHttpService } from 'src/driver-http/driver-http.service';
+import { OrderController } from './order.controller';
+import { Order } from './order.model';
 
-@Module({})
+@Module({
+  controllers: [OrderController],
+  imports: [TypeOrmModule.forFeature([Order]), HttpModule],
+  providers: [DriverHttpService],
+})
 export class OrderModule {}
