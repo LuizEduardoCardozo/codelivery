ee04f7a61b39d199daab6dbcc4e92e68fcefaf15
LuizEduardoCardozo <eduard.cardoz@gmail.com>
2021-07-18 03:43:39 -0300

    chore: adds a order controller

---
+++ mapping/back/src/order/order.controller.ts
@@ -0,0 +1,17 @@
+import { Controller, Get, Param } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Order } from './order.model';
+
+@Controller('order')
+export class OrderController {
+  constructor(
+    @InjectRepository(Order)
+    private readonly orderRepository: Repository<Order>,
+  ) {}
+
+  @Get('id')
+  async showById(@Param('id') id: string) {
+    return this.orderRepository.findOne(id);
+  }
+}
--- mapping/back/src/order/order.module.ts
+++ mapping/back/src/order/order.module.ts
@@ -4,6 +4,7 @@ import { Order } from './order.model';
 import { MappingService } from './mapping/mapping.service';
 import { NewOrderService } from './new-order/new-order.service';
 import { RabbitMQModule } from '@golevelup/nestjs-rabbitmq';
+import { OrderController } from './order.controller';
 
 @Module({
   imports: [
@@ -13,5 +14,6 @@ import { RabbitMQModule } from '@golevelup/nestjs-rabbitmq';
     TypeOrmModule.forFeature([Order]),
   ],
   providers: [NewOrderService, MappingService],
+  controllers: [OrderController],
 })
 export class OrderModule {}
