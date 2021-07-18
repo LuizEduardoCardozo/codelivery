86032688fa761b0c484f2a19cc06ae9855a258bc
LuizEduardoCardozo <eduard.cardoz@gmail.com>
2021-07-18 02:59:19 -0300

    chore: add order model

---
+++ mapping/back/src/order/order.model.ts
@@ -0,0 +1,24 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+export enum OrderStatus {
+  PENDING = 1,
+  DONE = 2,
+}
+
+@Entity({ name: 'orders' })
+export class Order {
+  @PrimaryGeneratedColumn()
+  id: string;
+
+  @Column()
+  driver_name: string;
+
+  @Column()
+  location_id: number;
+
+  @Column('simple-array')
+  location_geo: number[];
+
+  @Column()
+  order_status: OrderStatus = OrderStatus.PENDING;
+}
