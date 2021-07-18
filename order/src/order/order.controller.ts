103ab03c0021d87541e25b0ba3aa66e3955d5281
LuizEduardoCardozo <eduard.cardoz@gmail.com>
2021-07-17 21:45:34 -0300

    chore: add order controller

---
+++ order/src/order/order.controller.ts
@@ -0,0 +1,32 @@
+import { Controller, Get, Render } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { DriverHttpService } from 'src/driver-http/driver-http.service';
+import { Repository } from 'typeorm';
+import { Order } from './order.model';
+
+@Controller('orders')
+export class OrderController {
+  constructor(
+    @InjectRepository(Order)
+    private readonly orderRepository: Repository<Order>,
+    private readonly driverService: DriverHttpService,
+  ) {}
+
+  @Get()
+  @Render('order/index')
+  async index() {
+    const orders = await this.orderRepository.find({
+      order: {
+        created_at: 'ASC',
+      },
+    });
+    return { data: orders };
+  }
+
+  @Get('/drivers')
+  @Render('order/index')
+  async drivers() {
+    const drivers = await this.driverService.getDrivers();
+    return { data: drivers };
+  }
+}
