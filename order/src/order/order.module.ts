abb9a8b1ebfc0d649c1df9f0c87756c94cbb3372
LuizEduardoCardozo <eduard.cardoz@gmail.com>
2021-07-18 01:09:51 -0300

    chore: add rabbit mq connection in correct providers imports in orders module

--- order/src/order/order.module.ts
+++ order/src/order/order.module.ts
@@ -1,12 +1,25 @@
+import { RabbitMQModule } from '@golevelup/nestjs-rabbitmq';
 import { HttpModule, Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ChangeStatusOrderService } from 'src/change-status-order/change-status-order.service';
 import { DriverHttpService } from 'src/driver-http/driver-http.service';
+import { OrderSubscriberService } from 'src/order-subscriber/order-subscriber.service';
 import { OrderController } from './order.controller';
 import { Order } from './order.model';
 
 @Module({
+  imports: [
+    RabbitMQModule.forRoot(RabbitMQModule, {
+      uri: `amqp://admin:admin@localhost:5672`,
+    }),
+    TypeOrmModule.forFeature([Order]),
+    HttpModule,
+  ],
   controllers: [OrderController],
-  imports: [TypeOrmModule.forFeature([Order]), HttpModule],
-  providers: [DriverHttpService],
+  providers: [
+    DriverHttpService,
+    ChangeStatusOrderService,
+    OrderSubscriberService,
+  ],
 })
 export class OrderModule {}
